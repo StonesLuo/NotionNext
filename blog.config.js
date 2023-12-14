32095ad5994c8fbb712423b85507a06e81284646
LeiLuo <stonesluo@gmail.com>
2023-12-14 11:36:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -216,14 +216,14 @@ const BLOG = {
       url: 'https://music.163.com/song/media/outer/url?id=2031184412.mp3',
       cover:
             'http://p2.music.126.net/6WUfbJUhabTFFcQ112hbgQ==/109951168476506301.jpg'
-    }
+    },
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
       cover:
             'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    },
+    }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
